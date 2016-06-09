68953ff32bf298663ede2315d99f34481c54178a
Pavel Grinchenko <psdcoder@gmail.com>
2016-06-09 18:09:57 +0300

    Refactored all code, wrote tests, setup prepublish npm script with build different package versions

---
+++ src/index.js
@@ -0,0 +1,100 @@
+const DEFAULT_OPTIONS = {
+    prevNextLinks: true,
+    dots: '...',
+    prevText: 'Prev',
+    nextText: 'Next',
+    afterFirst: 2,
+    beforeLast: 2,
+    beforeCurrent: 2,
+    afterCurrent: 2
+};
+
+function isNumber(value) {
+    return typeof value === 'number';
+}
+
+function createElement(text, page = null, isCurrent = false, isDisabled = false) {
+    return { text, page, isCurrent, isDisabled };
+}
+
+export default function paginationBuilder(currentPage, totalPages, options = {}) {
+    if (!isNumber(currentPage)) {
+        throw new Error('"currentPage" param must be Number');
+    }
+
+    if (!isNumber(totalPages)) {
+        throw new Error('"totalPages" param must be Number');
+    }
+
+    const result = [];
+    const opts = Object.assign({}, DEFAULT_OPTIONS, options);
+    let i;
+    let ii;
+
+    if (totalPages > 0) {
+        // Calculate dots
+        const currentRangeStart = currentPage - opts.beforeCurrent;
+        const currentRangeEnd = currentPage + opts.afterCurrent;
+        const leftDots = currentRangeStart - opts.afterFirst > 2 && opts.dots ? 1 : 0;
+        const rightDots = currentRangeEnd + opts.beforeLast < totalPages && opts.dots ? 1 : 0;
+
+        // Previous page item
+        if (opts.prevNextLinks && currentPage > 1) {
+            result.push(createElement(opts.prevText, currentPage - 1));
+        }
+
+        // First page
+        result.push(createElement('1', 1, currentPage === 1));
+
+        // Items after first page
+        if (currentRangeStart - leftDots > 1) {
+            for (i = 2, ii = i + opts.afterFirst; i < ii; i++) {
+                if (i >= currentRangeStart + leftDots) {
+                    break;
+                }
+
+                result.push(createElement(i.toString(), i));
+            }
+        }
+
+        // Left dots
+        if (leftDots) {
+            result.push(createElement(opts.dots, null, false, true));
+        }
+
+        // Items between dots
+        for (i = currentRangeStart; i <= currentRangeEnd; i++) {
+            if (i > 1 + leftDots && i < totalPages - rightDots) {
+                result.push(createElement(i.toString(), i, i === currentPage));
+            }
+        }
+
+        // Right dots
+        if (rightDots) {
+            result.push(createElement(opts.dots, null, false, true));
+        }
+
+        // Items before last page
+        if (currentRangeEnd + rightDots < totalPages) {
+            for (i = totalPages - opts.beforeLast, ii = i + opts.beforeLast; i < ii; i++) {
+                if (i <= currentRangeEnd + rightDots) {
+                    break;
+                }
+
+                result.push(createElement(i.toString(), i));
+            }
+        }
+
+        // Last page
+        if (totalPages > 1) {
+            result.push(createElement(totalPages.toString(), totalPages, currentPage === totalPages));
+        }
+
+        // Next page item
+        if (opts.prevNextLinks && currentPage < totalPages) {
+            result.push(createElement(opts.nextText, currentPage + 1));
+        }
+    }
+
+    return result;
+}
---
+++ test/pagination-builder.spec.js
@@ -0,0 +1,94 @@
+import { expect } from 'chai';
+import paginationBuilder from '../src';
+
+describe('paginationBuilder', () => {
+    it('Should throw on non number "currentPage" param', () => {
+        expect(() => paginationBuilder('1')).to.throw(/currentPage/);
+    });
+
+    it('Should throw on non number "totalPages" param', () => {
+        expect(() => paginationBuilder(1, '20')).to.throw(/totalPages/);
+    });
+
+    it('Should add next && prev links by default', () => {
+        const result = paginationBuilder(3, 10);
+        expect(result[0]).to.have.property('text', 'Prev');
+        expect(result[result.length - 1]).to.have.property('text', 'Next');
+    });
+
+    it('Should add dots', () => {
+        const dotsItems = paginationBuilder(3, 10).filter(item => item.text === '...');
+
+        expect(!!dotsItems.length).to.equal(true);
+    });
+
+    describe('custom options', () => {
+        it('disable nextPrevLinks rendering', () => {
+            const result = paginationBuilder(3, 10, { prevNextLinks: false });
+            expect(result[0]).to.not.have.property('text', 'Prev');
+            expect(result[result.length - 1]).to.not.have.property('text', 'Next');
+        });
+
+        it('Custom dots text', () => {
+            const customDots = '_ _ _';
+            const result = paginationBuilder(3, 10, { dots: customDots });
+            expect(result[6]).to.have.property('text', customDots);
+        });
+
+        it('Disabled dots', () => {
+            const dotsItems = paginationBuilder(3, 10, { dots: false }).filter(item => item.text === '...');
+
+            expect(!!dotsItems.length).to.equal(false);
+        });
+
+        it('Custom "prevText" option', () => {
+            const prevText = 'Previous text!';
+            const result = paginationBuilder(3, 10, { prevText });
+            expect(result[0]).to.have.property('text', prevText);
+        });
+
+        it('Custom "nextText" option', () => {
+            const nextText = 'Next text!';
+            const result = paginationBuilder(3, 10, { nextText });
+            expect(result[result.length - 1]).to.have.property('text', nextText);
+        });
+
+        it('Custom "afterFirst" option', () => {
+            const afterFirst = 1;
+            const result = paginationBuilder(10, 20, { afterFirst });
+            const firstPageIndex = result.findIndex(item => item.page === 1);
+            const firstDotsIndex = result.findIndex(item => item.text === '...');
+
+            expect(firstDotsIndex - firstPageIndex).to.equal(afterFirst + 1);
+        });
+
+        it('Custom "beforeLast" option', () => {
+            const beforeLast = 5;
+            const last = 40;
+            const result = paginationBuilder(10, last, { beforeLast });
+            const lastPageIndex = result.findIndex(item => item.page === last);
+            const lastDotsIndex = result.findIndex(item => item === result.filter(fItem => fItem.text === '...')[1]);
+
+            expect(lastPageIndex - lastDotsIndex).to.equal(beforeLast + 1);
+        });
+
+
+        it('Custom "beforeCurrent" option', () => {
+            const beforeCurrent = 4;
+            const result = paginationBuilder(15, 30, { beforeCurrent });
+            const currentIndex = result.findIndex(item => item.isCurrent);
+            const firstDotsIndex = result.findIndex(item => item.text === '...');
+
+            expect(currentIndex - firstDotsIndex).to.equal(beforeCurrent + 1);
+        });
+
+        it('Custom "afterCurrent" option', () => {
+            const afterCurrent = 10;
+            const result = paginationBuilder(15, 40, { afterCurrent });
+            const currentIndex = result.findIndex(item => item.isCurrent);
+            const lastDotsIndex = result.findIndex(item => item === result.filter(fItem => fItem.text === '...')[1]);
+
+            expect(lastDotsIndex - currentIndex).to.equal(afterCurrent + 1);
+        });
+    });
+});
